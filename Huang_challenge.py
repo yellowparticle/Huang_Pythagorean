ff675dfc81f70f7933e30a606e3c93222718cdea
Emily Huang <emilyhua.11573@redlandsschools.net>
2018-09-18 06:07:41 +0000

    pytha

--- Huang_challenge.py
+++ Huang_challenge.py
@@ -4,9 +4,9 @@ b=float(input("b: "))
 c=float(input("c: "))
 if (a+b<c):
     print("The lengths entered do not make a right triangle!")
-if (a**2 + b**2 == c**2):
+elif (a**2 + b**2 == c**2):
     print("The lengths entered make a right triangle!")
-if (a**2 + b**2 < c**2):
+elif (a**2 + b**2 < c**2):
     print("The lengths entered make an obtuse triangle!")
-if (a**2 + b**2 > c**2):
+elif (a**2 + b**2 > c**2):
     print("The lengths entered make an acute triangle!")
